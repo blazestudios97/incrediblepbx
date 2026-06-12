9c4cd4eb8298678c6e4435f1913acbe300344c0f
Tom Ray <tom.ray@blazestudios.com>
2026-06-11 21:05:36 -0400

    adding pbxstatus for changes tracking

--- IncrediblePBX2026.sh
+++ IncrediblePBX2026.sh
@@ -181,15 +181,7 @@ a2enmod rewrite
 systemctl restart apache2
 rm /var/www/html/index.html
 
-
-
-
-
 install_setup_freepbx
-
-
-
-
 
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED VIA unix_socket;"
 mysql -u root -e "FLUSH PRIVILEGES;"
@@ -226,20 +218,18 @@ publicip=$(
   curl -4 -s https://ifconfig.me ||
   curl -4 -s https://checkip.amazonaws.com
 )
-
+ipset add trusted_hosts "$serverip" -exist
+ipset add trusted_hosts "$userip" -exist
+ipset add trusted_hosts "$publicip" -exist
 # WhiteList all of them by replacing 8.8.4.4 and 8.8.8.8 and 74.86.213.25 entries
 cp /etc/iptables/rules.v4 /etc/iptables/rules.v4.orig
 cd /etc/iptables
 cp /etc/iptables/rules.v4 /etc/iptables/rules.v4.orig
-wget http://incrediblepbx.com/iptables4-ubuntu18.04.2.tar.gz
-tar zxvf iptables4-ubuntu18.04.2.tar.gz
-rm iptables4-ubuntu18.04.2.tar.gz
-mv iptables-custom /usr/local/sbin
+
+
 mv openssl.cnf /etc/ssl
-cp rules.v4.tm3 rules.v4
-sed -i 's|8.8.4.4|'$serverip'|' /etc/iptables/rules.v4
-sed -i 's|8.8.8.8|'$userip'|' /etc/iptables/rules.v4
-sed -i 's|74.86.213.25|'$publicip'|' /etc/iptables/rules.v4
+cp rules.v4.tm4 rules.v4
+
 
 badline=`grep -n "\-s  \-p" /etc/iptables/rules.v4 | cut -f1 -d: | tail -1`
 while [[ "$badline" != "" ]]; do
@@ -247,26 +237,12 @@ sed -i "${badline}d" /etc/iptables/rules.v4
 badline=`grep -n "\-s  \-p" /etc/iptables/rules.v4 | cut -f1 -d: | tail -1`
 done
 sed -i 's|-A INPUT -s  -j|#-A INPUT -s  -j|g' /etc/iptables/rules.v4
-#sed -i 's|#-A INPUT -p tcp -m tcp --dport 22|-A INPUT -p tcp -m tcp --dport 22|' rules.v4
-ln -s /etc/init.d/iptables-persistent /etc/init.d/iptables
-echo "/usr/sbin/iptables -A INPUT -s 100.64.0.0/10 -j ACCEPT" >> /usr/local/sbin/iptables-custom
-
-sed -i 's|/usr/sbin/iptables -A INPUT -p udp -m udp -s sip1.v1voip|#/usr/sbin/iptables -A INPUT -p udp -m udp -s sip1.v1voip|' /usr/local/sbin/iptables-custom
-sed -i 's|/usr/sbin/iptables -A INPUT -p udp -m udp -s sip2.v1voip|#/usr/sbin/iptables -A INPUT -p udp -m udp -s sip2.v1voip|' /usr/local/sbin/iptables-custom
-sed -i 's|/usr/sbin/iptables -A INPUT -p udp -m udp -s sip.v1voip|#/usr/sbin/iptables -A INPUT -p udp -m udp -s sip.v1voip|' /usr/local/sbin/iptables-custom
-sed -i 's|/usr/sbin/iptables -A INPUT -s sip.nexmo.com|#/usr/sbin/iptables -A INPUT -s sip.nexmo.com|' /usr/local/sbin/iptables-custom
-sed -i 's|/usr/sbin/iptables -I INPUT -s api.nexmo.com|#/usr/sbin/iptables -I INPUT -s api.nexmo.com|' /usr/local/sbin/iptables-custom
-sed -i 's|/usr/sbin/iptables -A INPUT -s sip.signalwire|#/usr/sbin/iptables -A INPUT -s sip.signalwire|' /usr/local/sbin/iptables-custom
 
 /usr/local/sbin/iptables-custom
 systemctl restart iptables
 /usr/local/sbin/iptables-custom
 
-cd /
-wget http://incrediblepbx.com/rootfiles-debian10.tar.gz
-tar zxvf rootfiles-debian10.tar.gz
-rm rootfiles-debian10.tar.gz
-chattr -i /root/up*
+
 
 cd /usr/local/sbin
 wget https://filedn.com/lBgbGypMOdDm8PWOoOiBR7j/images/pbxstatus-2027D
@@ -287,8 +263,7 @@ sed -i 's|fail2ban-client -x start|#fail2ban-client -x start|' /etc/fail2ban/pat
 systemctl enable fail2ban
 systemctl start fail2ban
 
-# remove CentOS fax installer
-rm -f /root/incrediblefax*
+
 
 
 
@@ -324,8 +299,7 @@ else
  ln -s /var/lib/asterisk/bin/fwconsole /usr/sbin/fwconsole
 fi
 
-rm -f /root/upgrade-asterisk16
-rm -f /root/upgrade-asterisk18
+
 sed -i 's|7.3|8.2|' /root/timezone-setup
 
 
@@ -347,24 +321,8 @@ fwconsole chown
 fwconsole reload
 fwconsole restart
 
-echo '[Unit]
-Description=openvpn2027
-ConditionPathExists=/etc/openvpn-start
-After=rclocal.service
-[Service]
-Type=forking
-ExecStart=/etc/openvpn-start /etc/incrediblepbx2027.ovpn
-TimeoutSec=0
-StandardOutput=tty
-RemainAfterExit=yes
-PermissionsStartOnly=true
-SysVStartPriority=99
-[Install]
-WantedBy=multi-user.target' > /etc/systemd/system/openvpn2027.service
-chmod +x /etc/systemd/system/openvpn2027.service
-cp -p /root/openvpn-start /etc/openvpn-start
-systemctl enable openvpn2027.service
-systemctl restart openvpn2027.service
+
+
 
 # Watson STT fix
 sed -i 's|/usr/local/sbin/we-dont-have-tech-support.wav|/var/lib/asterisk/sounds/en/we-dont-have-tech-support.gsm|' /usr/local/sbin/watson-test
@@ -405,37 +363,7 @@ if [ -z "$test" ]; then
 fi
 
 
-install -m 640 files/knockd/knockd.conf /etc/knockd.conf
-# randomize ports here
-lowest=6001
-highest=9950
-knock1=$[ ( $RANDOM % ( $[ $highest - $lowest ] + 1 ) ) + $lowest ]
-knock2=$[ ( $RANDOM % ( $[ $highest - $lowest ] + 1 ) ) + $lowest ]
-knock3=$[ ( $RANDOM % ( $[ $highest - $lowest ] + 1 ) ) + $lowest ]
-sed -i 's|7:udp|'$knock1':tcp|' /etc/knockd.conf
-sed -i 's|8:udp|'$knock2':tcp|' /etc/knockd.conf
-sed -i 's|9:udp|'$knock3':tcp|' /etc/knockd.conf
-systemctl restart knockd
-systemctl enable knockd
-echo " "
-echo "Knock ports for access to $publicip set to TCP: $knock1 $knock2 $knock3" > /root/knock.FAQ
-echo "UPnP activation attempted for UDP 5060 and your knock ports above." >> /root/knock.FAQ
-echo "To enable knockd on your server, issue the following commands:" >> /root/knock.FAQ
-echo "  chkconfig --level 2345 knockd on" >> /root/knock.FAQ
-echo "  service knockd start" >> /root/knock.FAQ
-echo "To enable remote access, issue these commands after yum -y install nmap:" >> /root/knock.FAQ
-echo "nmap -p $knock1 --max-retries 0 $publicip && nmap -p $knock2 --max-retries 0 $publicip && nmap -p $knock3 --max-retries 0 $publicip" >> /root/knock.FAQ
-echo "Or install iOS PortKnock or Android DroidKnocker on remote device." >> /root/knock.FAQ
 
-echo "#!/bin/sh -e
-service knockd start
-sleep 5
-/usr/local/sbin/iptables-restart
-sleep 30
-fwconsole restart
-exit 0
-" > /etc/rc.local
-chattr +i /etc/rc.local
 
 sed -i 's|; max_input_vars = 1000|max_input_vars = 5000|' /etc/php/8.2/apache2/php.ini
 # systemctl restart apache2.service
@@ -491,10 +419,7 @@ exten => s,n,Hangup
 
 ###  Add the update checker program  ###
 
-cd /root
-wget https://filedn.com/lBgbGypMOdDm8PWOoOiBR7j/Debian12/root-folder-update.tar.gz
-tar zxvf root-folder-update.tar.gz
-rm -f root-folder-update.tar.gz
+
 
 ### Get rid of Sendmail and Add Postfix ###
 
@@ -510,61 +435,12 @@ chattr +i /etc/rc.local
 
 ### Set up log rotation for Asterisk log files ###
 touch /etc/logrotate.d/asterisk
-echo "/var/log/asterisk/queue_log {
-su asterisk asterisk
-daily
-missingok
-rotate 30
-notifempty
-sharedscripts
-create 0640 asterisk asterisk
-}
 
-/var/log/asterisk/freepbx_dbug{
-su asterisk asterisk
-size 500M
-missingok
-rotate 7
-notifempty
-sharedscripts
-create 0640 asterisk asterisk
-}
 
-/var/log/asterisk/prosody_debug.log
-/var/log/asterisk/prosody.log
-/var/log/asterisk/ucp_err.log
-/var/log/asterisk/ucp_forever.log
-/var/log/asterisk/ucp_out.log
-/var/log/asterisk/freepbx_debug
-/var/log/asterisk/freepbx.log
-/var/log/asterisk/freepbx_security.log {
-su asterisk asterisk
-size 100M
-missingok
-rotate 7
-notifempty
-sharedscripts
-create 0640 asterisk asterisk
-}
 
-/var/spool/mail/asterisk
-/var/log/asterisk/messages
-/var/log/asterisk/event_log
-/var/log/asterisk/full
-/var/log/asterisk/dtmf
-/var/log/asterisk/fail2ban {
-su asterisk asterisk
-daily
-missingok
-rotate 7
-notifempty
-sharedscripts
-create 0640 asterisk asterisk
-postrotate
-/usr/sbin/asterisk -rx ‘logger reload’ > /dev/null 2> /dev/null
-endscript
-}
-" > /etc/logrotate.d/asterisk
+
+
+
 
 
 /root/admin-pw-change
@@ -576,14 +452,14 @@ fwconsole reload
 
 
 
-sed -i "s|deb http|deb [trusted=yes] http|" /etc/apt/sources.list
 
 cd /
 wget http://incrediblepbx.com/ipbx2024.tar.gz
 tar zxvf ipbx2024.tar.gz
 wget https://filedn.com/lBgbGypMOdDm8PWOoOiBR7j/Debian12/iPBX-custom.tar.gz
 tar zxvf iPBX-custom.tar.gz
-sed -i 's|bookworm|trixie|' /etc/apt/sources.list
+
+
 apt-get update
 fwconsole reload
 
@@ -823,8 +699,8 @@ install_messaging_packages() {
 }
 install_utility_packages() {
     install_packages \
-    jq \
-    cron
+        jq \
+        cron
 }
 install_apache_extras() {
     install_packages \
@@ -932,7 +808,10 @@ fwconsole setting FPBX_ARI_USER "$ARI_USER"
 fwconsole setting FPBX_ARI_PASSWORD "$ARI_PASS"
 asterisk -rx "database put blacklist dest app-blackhole,no-service,1"
 }
+
+
 install_incrediblepbx() {
+
 wget https://filedn.com/lBgbGypMOdDm8PWOoOiBR7j/Debian12/incrediblepbx-17.0.0.tgz
     #fwconsole ma install incrediblepbx
 
@@ -961,4 +840,88 @@ fwconsole ma downloadinstall https://filedn.com/lBgbGypMOdDm8PWOoOiBR7j/Incredib
 ./sig-fix
 ./sig-fix
 
+}
+
+setup_rootfiles() {
+cd /
+wget http://incrediblepbx.com/rootfiles-debian10.tar.gz
+tar zxvf rootfiles-debian10.tar.gz
+rm rootfiles-debian10.tar.gz
+chattr -i /root/up*
+}
+
+setup_knockd() {
+    CONF="/etc/default/knockd"
+
+    # Enable knockd
+    sed -i 's/^START_KNOCKD=0/START_KNOCKD=1/' "$CONF"
+
+    # Get first non-loopback interface that is UP
+    devport=$(ip -o link show up | awk -F': ' '{print $2}' | grep -v lo | head -n 1)
+
+    # Fallback safety
+    if [ -z "$devport" ]; then
+        echo "No active interface found, defaulting to eth0"
+        devport="eth0"
+    fi
+
+    # Remove existing KNOCKD_OPTS line (avoid duplicates)
+    sed -i '/^KNOCKD_OPTS=/d' "$CONF"
+
+    # Add new config
+    echo "KNOCKD_OPTS=\"-i $devport\"" >> "$CONF"
+
+    install -m 640 files/knockd/knockd.conf /etc/knockd.conf
+    # randomize ports here
+    lowest=6001
+    highest=9950
+    knock1=$[ ( $RANDOM % ( $[ $highest - $lowest ] + 1 ) ) + $lowest ]
+    knock2=$[ ( $RANDOM % ( $[ $highest - $lowest ] + 1 ) ) + $lowest ]
+    knock3=$[ ( $RANDOM % ( $[ $highest - $lowest ] + 1 ) ) + $lowest ]
+    sed -i 's|7:udp|'$knock1':tcp|' /etc/knockd.conf
+    sed -i 's|8:udp|'$knock2':tcp|' /etc/knockd.conf
+    sed -i 's|9:udp|'$knock3':tcp|' /etc/knockd.conf
+    systemctl restart knockd
+    systemctl enable knockd
+    echo " "
+    echo "Knock ports for access to $publicip set to TCP: $knock1 $knock2 $knock3" > /root/knock.FAQ
+    echo "UPnP activation attempted for UDP 5060 and your knock ports above." >> /root/knock.FAQ
+    echo "To enable knockd on your server, issue the following commands:" >> /root/knock.FAQ
+    echo "  chkconfig --level 2345 knockd on" >> /root/knock.FAQ
+    echo "  service knockd start" >> /root/knock.FAQ
+    echo "To enable remote access, issue these commands after yum -y install nmap:" >> /root/knock.FAQ
+    echo "nmap -p $knock1 --max-retries 0 $publicip && nmap -p $knock2 --max-retries 0 $publicip && nmap -p $knock3 --max-retries 0 $publicip" >> /root/knock.FAQ
+    echo "Or install iOS PortKnock or Android DroidKnocker on remote device." >> /root/knock.FAQ
+
+echo "#!/bin/sh -e
+service knockd start
+sleep 5
+/usr/local/sbin/iptables-restart
+sleep 30
+fwconsole restart
+exit 0
+" > /etc/rc.local
+chattr +i /etc/rc.local
+}
+
+setup_firewall() {
+    echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+    echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+    
+    ipset create trusted_hosts hash:ip -exist
+    ipset create trusted_providers hash:ip -exist
+
+    systemctl enable ipset-restore
+    systemctl enable --now ipset-dns-refresh.timer
+}
+
+setup_fail2ban() {
+
+}
+
+setup_openvpn() {
+    chmod +x /etc/systemd/system/openvpn2027.service
+cp -p /root/openvpn-start /etc/openvpn-start
+systemctl enable openvpn2027.service
+systemctl restart openvpn2027.service
 }
